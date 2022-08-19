e71058b85ce802df46fad3e30286f2c822b5828f
Artem Pasko <guramibox777@gmail.com>
2022-08-19 20:27:36 +0300

    added task solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        final String DELIMITER = ":";
+        String[] data = {};
+        try (java.io.FileReader fileReader = new java.io.FileReader(file)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int ch;
+            while ((ch = fileReader.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            data = stringBuilder.toString().split("\r");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(
+                (data[0].split(DELIMITER))[1].trim(),
+                Integer.parseInt((data[1].split(DELIMITER))[1].trim()),
+                (data[2].split(DELIMITER))[1].trim(),
+                Long.parseLong((data[3].split(DELIMITER))[1].trim()));
     }
 }
