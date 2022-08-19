63d40b3e4864b19755f2db9175e9e67b0cb9a226
Artem Pasko <guramibox777@gmail.com>
2022-08-19 21:17:05 +0300

    fixed issues

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,31 +2,41 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        final String DELIMITER = ":";
-        String[] data = {};
+        Map<String, String> dataMap = new HashMap<>();
         try (java.io.FileReader fileReader = new java.io.FileReader(file)) {
             StringBuilder stringBuilder = new StringBuilder();
             int ch;
             while ((ch = fileReader.read()) != -1) {
                 stringBuilder.append((char) ch);
             }
-            data = stringBuilder.toString().split("\r");
+            dataMap = getMapFromString(stringBuilder.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(data.length <= 1) {
+        if(dataMap.isEmpty()) {
             return new Profile();
         } else {
-            return new Profile(
-                    (data[0].split(DELIMITER))[1].trim(),
-                    Integer.parseInt((data[1].split(DELIMITER))[1].trim()),
-                    (data[2].split(DELIMITER))[1].trim(),
-                    Long.parseLong((data[3].split(DELIMITER))[1].trim()));
+            return new Profile(dataMap.get("Name"),
+                    Integer.parseInt(dataMap.get("Age")),
+                    dataMap.get("Email"),
+                    Long.parseLong(dataMap.get("Phone")));
         }
+    }
+
+    private Map<String, String> getMapFromString(String str){
+        Map<String, String> resultMap = new HashMap<>();
+        String[] data = str.split("\r\n");
+        for(String el : data) {
+            String[] dataArray = el.split(":");
+            resultMap.put(dataArray[0].trim(), dataArray[1].trim());
+        }
+        return resultMap;
     }
 }
